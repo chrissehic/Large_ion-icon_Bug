47439956c31dff7a7fadf6268321d149901081f6
Christopher Sehic <christopher.sehic@gmail.com>
2020-06-02 18:30:58 +0200

    test app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
+  constructor(public navCtrl: NavController) {}
 
-  constructor() {}
+  ngOnInit() {}
 
+  openContent() {
+    this.navCtrl.navigateForward("extras", { animated: false });
+
+  
+  }
 }
